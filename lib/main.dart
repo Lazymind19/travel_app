3c2e940b0d1bb6a8eedc125802114b748d83d9eb
Lazy Mind <srjkadel@gmail.com>
2022-07-17 11:46:04 +0545

    feat : added cubit state, logic etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/cubit/app_cubit.dart';
+import 'package:travel_app/cubit/app_cubit_logic.dart';
 import 'package:travel_app/pages/detail_page.dart';
 import 'package:travel_app/pages/home_page.dart';
 import 'package:travel_app/pages/nav_pages/main_page.dart';
@@ -23,7 +24,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:MainPage());
+      home:BlocProvider(
+        create: (context) => AppCubits(),
+        child: AppCubitLogics(),
+      ));
 
   }
 }
